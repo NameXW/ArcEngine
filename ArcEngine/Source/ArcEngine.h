749a939314b544719c158aa3dc38fb469849164e
NAME_XW <zex612@outlook.com>
2025-12-10 01:20:37 +0800

    Setup basic application and entry point

---
+++ ArcEngine/Source/ArcEngine.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// For use by ArcEngine applications
+
+#include "stdio.h"
+#include "ArcEngine/Application.h"
+#include "ArcEngine/EntryPoint.h"
---
+++ Sandbox/Source/SandboxApp.cpp
@@ -0,0 +1,21 @@
+#include <ArcEngine.h>
+
+class Sandbox : public Arc::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+	
+	~Sandbox()
+	{
+
+	}
+
+};
+
+Arc::Application* Arc::CreateApplication()
+{
+	return new Sandbox();
+}
